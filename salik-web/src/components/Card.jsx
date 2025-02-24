2c2d22a1ab7d953fcb37c47452ea5b319bfd73b8
Mohamed <mohamedelgohary410@gmail.com>
2025-02-24 22:44:30 +0200

    handle text field cases

--- salik-web/src/components/Card.jsx
+++ salik-web/src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, CardContent, Typography, Box } from "@mui/material";
+import { Card, CardContent, Typography, Box, colors } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import {
   cancelRideAction,
@@ -12,7 +12,7 @@ import car from "/images/car.png"; // Correct path
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { IconButton, Tooltip } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { data, useNavigate } from "react-router-dom";
 const Cards = ({ ride }) => {
   const { user } = useSelector((state) => state.auth)
   console.log("User type: ", user.type);
@@ -28,15 +28,13 @@ const Cards = ({ ride }) => {
     await dispatch(deleteRideAction(ride._id));
     dispatch(fetchProvidedRides());
   }
-  // const handleEdit = () => {
-  //   console.log("Edit ride:", ride._id);
-  //   // Add your edit logic here
-  // };
-
   // Format ride date and time
   const rideDate = ride?.rideDateTime?.split("T")[0];
   const rideTime = ride?.rideDateTime?.split("T")[1]?.slice(0, 5);
-
+  const handleLocationField = (data) => {
+    var location = data?.length > 13 ? `${data?.slice(0, 13)}...` : data;
+    return location;
+  }
   return (
     <Box display="flex" alignItems="center" gap={2} mb={2}>
       {/* Left side */}
@@ -45,7 +43,7 @@ const Cards = ({ ride }) => {
           sx={{
             width: 12,
             height: 12,
-            bgcolor: cancelled ? "grey" : "#FFB800",
+            bgcolor: { colors },
             borderRadius: "50%",
             marginRight: 1,
             boxShadow: 1,
@@ -57,7 +55,7 @@ const Cards = ({ ride }) => {
       </Box>
 
       {/* Ride Card */}
-      <Card sx={{ width: 500, display: "flex", alignItems: "center", padding: 1 }}>
+      <Card sx={{ width: "fit-content", display: "flex", alignItems: "center", padding: 1 }}>
         <CardContent sx={{ display: "flex", alignItems: "center", width: "100%" }}>
           {/* Car Icon */}
           <img src={car} style={{ width: 50, marginRight: 20 }} alt="Car" />
@@ -65,7 +63,7 @@ const Cards = ({ ride }) => {
           {/* Ride Details */}
           <Box flexGrow={1}>
             <Typography variant="subtitle1">
-              {ride.fromLocation} to {ride.toLocation}
+              {handleLocationField(ride.fromLocation)} to {handleLocationField(ride.toLocation)}
             </Typography>
             <Typography variant="body2">
               Price: {ride.price} $ &nbsp; &nbsp; Seats: {user.type === "customer" ? ride.bookedSeats : ride.totalSeats - ride.bookedSeats}
@@ -78,17 +76,16 @@ const Cards = ({ ride }) => {
               variant="contained"
               disabled={cancelled}
               sx={{
-                backgroundColor: cancelled ? "grey" : "#FFB800",
+                backgroundColor: "#FFB800",
                 color: "white",
-                textDecoration: cancelled ? "line-through" : "none",
-                pointerEvents: cancelled ? "none" : "auto",
+                pointerEvents: "auto",
                 "&:hover": {
-                  backgroundColor: cancelled ? "grey" : "#FFA500",
+                  backgroundColor: colors,
                 },
               }}
               onClick={handleCancel}
             >
-              {cancelled ? "Cancelled" : "Cancel"}
+              Cancel
             </MainButton>
           )}
 
