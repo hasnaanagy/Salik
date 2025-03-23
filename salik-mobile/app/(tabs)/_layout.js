c3d2a5b4c17e34f024c02145d06161b40646c120
nermeenelmasry286 <112644208+nermeenelmasry286@users.noreply.github.com>
2025-03-24 00:10:09 +0200

    add drawer

--- salik-mobile/app/(tabs)/_layout.js
+++ salik-mobile/app/(tabs)/_layout.js
@@ -1,25 +1,52 @@
+// app/(tabs)/_layout.jsx
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { TabBar } from '../../components/SharedComponents/TabBar';
 import { Platform } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import ProfileComponent from '../../components/UserComponents/profileComponent';
+
+const Drawer = createDrawerNavigator();
 
 const TabsLayout = () => {
+  const options = {
+    headerStyle: {
+      backgroundColor: 'transparent',
+      elevation: 0,
+      shadowOpacity: 0,
+      height: Platform.OS === 'ios' ? 100 : 60
+    },
+    headerTitleAlign: "left",
+    headerTitleStyle: {
+      fontFamily: "Poppins_400Regular",
+      fontSize: 34,
+    }
+  };
 
-    const options={
-        headerStyle:{backgroundColor:'transparent',elevation:0,shadowOpacity:0,height:Platform.OS==='ios'?100:60},
-        headerTitleAlign:'left',
-        headerTitleStyle:{
-                  fontFamily: 'Poppins_400Regular',
-                    fontSize: 34,
-    }}
-    return (
-       <Tabs tabBar={(props)=><TabBar {...props} />}>
-        <Tabs.Screen name='index' options={{title: 'Salik',...options}}/>
-        <Tabs.Screen name='requests' options={{title: 'Requests',...options}} />
-        <Tabs.Screen name='activity' options={{title: 'Activity',...options}} />
-        <Tabs.Screen name='profile' options={{title: 'Profile',...options}} />
-       </Tabs>
-    );
-}
+  return (
+    <Tabs tabBar={(props) => <TabBar {...props} />}>
+      <Tabs.Screen name="index" options={{ title: "Salik", ...options }} />
+      <Tabs.Screen name="requests" options={{ title: "Requests", ...options }} />
+      <Tabs.Screen name="activity" options={{ title: "Activity", ...options }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile", ...options }} />
+    </Tabs>
+  );
+};
 
-export default TabsLayout;
+const DrawerLayout = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <ProfileComponent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          width: 300,
+        },
+      }}
+    >
+      <Drawer.Screen name="tabs" component={TabsLayout} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerLayout;
